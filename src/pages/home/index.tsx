2a4337a6dc02a10d3877b07b24928e0c678d6890
geqb <geqb@shanhai.team>
2022-10-19 17:18:28 +0800

    优化背景清晰度

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import "./index.css"
+import "./index.scss"
 import { useEffect, Component } from "react"
 import * as config from "src/config"
 import * as PIXI from "pixi.js"
@@ -20,169 +20,155 @@ export default class Home extends Component {
         right: false,
     }
 
+    private app: any
     private spineBoy: any
+    private bgContainer: any
+    private bgTexture5: any
 
     componentDidMount() {
-        this.init1()
+        this.initApp()
     }
 
-    init1() {
-        const app = new PIXI.Application({
-            width: 1768,
-            height: 1110,
+    initApp() {
+        this.app = new PIXI.Application({
+            width: config.clientWidth(),
+            height: config.clientHeight(),
             antialias: true, // 抗锯齿
-            resolution: 1, // 分辨率
+            resolution: 2, // 分辨率
             transparent: false, // 背景透明
             backgroundColor: 0xd7dd7d,
         })
 
-        document.getElementById("main")?.appendChild(app.view)
+        document.getElementById("main")?.appendChild(this.app.view)
+        this.app.stop()
 
-        app.stop()
+        this.bgFn()
 
-        const bgContainer = new PIXI.Container()
+        this.app.stage.addChild(this.bgContainer)
 
-        // const bgContaineScale = 0.5
-        // bgContainer.scale.set(bgContaineScale)
+        this.app.stage.pivot.x = (this.bgTexture5.width - config.clientWidth()) / 2
+        this.app.stage.pivot.y = (this.bgTexture5.height - config.clientHeight()) / 2
+        this.app.stage.interactive = true
+        this.app.stage.buttonMode = true
 
-        app.stage.addChild(bgContainer)
+        this.loadPeople()
+        this.appTicker()
 
-        bgContainer.interactive = true
+        this.app.start()
+    }
 
-        // const bgTexture1 = new PIXI.Sprite(PIXI.Texture.from('assets/bg/left-top.jpg'))
-        // const bgTexture2 = new PIXI.Sprite(PIXI.Texture.from('assets/bg/left-bottom.jpg'))
-        // const bgTexture3 = new PIXI.Sprite(PIXI.Texture.from('assets/bg/right-top.jpg'))
-        // const bgTexture4 = new PIXI.Sprite(PIXI.Texture.from('assets/bg/right-bottom.jpg'))
+    // 背景
+    bgFn() {
+        this.bgContainer = new PIXI.Container()
 
-        const bgTexture5 = new PIXI.Sprite(PIXI.Texture.from(String(PUBLIC_PATH) + "pixi/bg/bg.png"))
+        this.bgContainer.interactive = true
 
-        // bgTexture1.position.set(0, 0)
+        this.bgTexture5Fn()
 
-        // bgTexture1.width = 1680 / 2
-        // bgTexture1.height = 1976 / 2
+        this.bgContainer
+            .on("pointertap", (event: any) => {
+                this.diff = {
+                    x: event.data.global.x - config.clientWidth() / 2,
+                    y: event.data.global.y - config.clientHeight() / 2,
+                }
 
-        // bgTexture2.position.set(0, 1976 / 2)
+                // bg的偏移量
+                this.oldBgContainerX = this.bgContainer.x
+                this.oldBgContainerY = this.bgContainer.y
 
-        // bgTexture2.width = 1680 / 2
-        // bgTexture2.height = 1606 / 2
+                if (event.data.global.x - config.clientWidth() / 2 > 0) {
+                    this.bgType.right = true
+                    this.bgType.left = false
+                    this.spineBoy.skew.set(0, 0)
+                }
 
-        // bgTexture3.position.set(1680 / 2, 0)
+                if (event.data.global.x - config.clientWidth() / 2 < 0) {
+                    this.bgType.right = false
+                    this.bgType.left = true
+                    this.spineBoy.skew.set(0, 3.2)
+                }
 
-        // bgTexture3.width = 2120 / 2
-        // bgTexture3.height = 1976 / 2
+                if (event.data.global.y - config.clientHeight() / 2 > 0) {
+                    this.bgType.top = true
+                    this.bgType.bottom = false
+                }
 
-        // bgTexture4.position.set(1680 / 2, 1976 /2)
+                if (event.data.global.y - config.clientHeight() / 2 < 0) {
+                    this.bgType.top = false
+                    this.bgType.bottom = true
+                }
 
-        // bgTexture4.width = 2120 / 2
-        // bgTexture4.height = 1606 / 2
+                this.dragging = true
+                this.data = event.data
+            })
+            .on("pointerup", () => {
+                this.dragging = false
+                this.data = null
+            })
+            .on("pointerupoutside", () => {
+                this.dragging = false
+                this.data = null
+            })
+            .on("pointermove", (event: any) => {
+                if (this.dragging) {
+                    // console.log(11)
+                }
+            })
+    }
 
-        bgTexture5.position.set(0, 0)
+    bgTexture5Fn() {
+        this.bgTexture5 = new PIXI.Sprite(PIXI.Texture.from(String(PUBLIC_PATH) + "pixi/bg/bg.jpg"))
 
-        bgTexture5.width = app.screen.width
-        bgTexture5.height = app.screen.height
+        this.bgTexture5.position.set(0, 0)
 
-        // bgContainer.addChild(bgTexture1)
-        // bgContainer.addChild(bgTexture2)
-        // bgContainer.addChild(bgTexture3)
-        // bgContainer.addChild(bgTexture4)
+        this.bgTexture5.width = 5304 / 4
+        this.bgTexture5.height = 3330 / 4
 
-        bgContainer.addChild(bgTexture5)
+        this.bgContainer.addChild(this.bgTexture5)
+    }
 
-        app.stage.pivot.x = (app.screen.width - window.screen.availWidth) / 2
-        app.stage.pivot.y = (app.screen.height - window.screen.availHeight) / 2
-
-        app.loader.add("spineboy", String(PUBLIC_PATH) + "pixi/spineboy.json").load((loader: any, res: any) => {
+    // 加载人物
+    loadPeople() {
+        this.app.loader.add("spineboy", String(PUBLIC_PATH) + "pixi/spineboy.json").load((loader: any, res: any) => {
             this.spineBoy = new Spine(res.spineboy.spineData)
 
             const spineBodyCoordinates = {
-                x: app.screen.width / 2,
-                y: app.screen.height / 2,
+                x: this.bgTexture5.width / 2,
+                y: this.bgTexture5.height / 2,
             }
 
             this.spineBoy.x = spineBodyCoordinates.x
             this.spineBoy.y = spineBodyCoordinates.y
-            this.spineBoy.scale.set(0.3)
+            this.spineBoy.scale.set(0.2)
 
             this.spineBoy.state.setAnimation(0, "walk", true)
-            app.stage.addChild(this.spineBoy)
-
-            bgContainer
-                .on("pointertap", event => {
-                    this.diff = {
-                        x: event.data.global.x - window.screen.availWidth / 2,
-                        y: event.data.global.y - window.screen.availHeight / 2,
-                    }
-
-                    // bg的偏移量
-                    this.oldBgContainerX = bgContainer.x
-                    this.oldBgContainerY = bgContainer.y
-
-                    if (event.data.global.x - window.screen.availWidth / 2 > 0) {
-                        this.bgType.right = true
-                        this.bgType.left = false
-                        this.spineBoy.skew.set(0, 0)
-                    }
-
-                    if (event.data.global.x - window.screen.availWidth / 2 < 0) {
-                        this.bgType.right = false
-                        this.bgType.left = true
-                        this.spineBoy.skew.set(0, 3.2)
-                    }
-
-                    if (event.data.global.y - window.screen.availHeight / 2 > 0) {
-                        this.bgType.top = true
-                        this.bgType.bottom = false
-                    }
-
-                    if (event.data.global.y - window.screen.availHeight / 2 < 0) {
-                        this.bgType.top = false
-                        this.bgType.bottom = true
-                    }
-
-                    this.dragging = true
-                    this.data = event.data
-                })
-                .on("pointerup", () => {
-                    this.dragging = false
-                    this.data = null
-                })
-                .on("pointerupoutside", () => {
-                    this.dragging = false
-                    this.data = null
-                })
-                .on("pointermove", event => {
-                    if (this.dragging) {
-                        // console.log(11)
-                    }
-                })
+            this.app.stage.addChild(this.spineBoy)
         })
+    }
 
-        app.stage.interactive = true
-        app.stage.buttonMode = true
-
-        app.ticker.add(delta => {
+    // 舞台ticker事件
+    appTicker() {
+        this.app.ticker.add((delta: any) => {
             if (this.diff.x && this.bgType !== "") {
                 // 横向走
-                if (this.bgType.right && Math.abs(bgContainer.x - this.oldBgContainerX) < this.diff.x) {
-                    bgContainer.x -= 0.8
+                if (this.bgType.right && Math.abs(this.bgContainer.x - this.oldBgContainerX) < this.diff.x) {
+                    this.bgContainer.x -= 1
                 }
 
-                if (this.bgType.left && Math.abs(this.oldBgContainerX - bgContainer.x) < Math.abs(this.diff.x)) {
-                    bgContainer.x += 0.8
+                if (this.bgType.left && Math.abs(this.oldBgContainerX - this.bgContainer.x) < Math.abs(this.diff.x)) {
+                    this.bgContainer.x += 1
                 }
 
                 // 垂直走
-                if (this.bgType.top && Math.abs(bgContainer.y - this.oldBgContainerY) < this.diff.y) {
-                    bgContainer.y -= 0.8
+                if (this.bgType.top && Math.abs(this.bgContainer.y - this.oldBgContainerY) < this.diff.y) {
+                    this.bgContainer.y -= 1
                 }
 
-                if (this.bgType.bottom && Math.abs(this.oldBgContainerY - bgContainer.y) < Math.abs(this.diff.y)) {
-                    bgContainer.y += 0.8
+                if (this.bgType.bottom && Math.abs(this.oldBgContainerY - this.bgContainer.y) < Math.abs(this.diff.y)) {
+                    this.bgContainer.y += 1
                 }
             }
         })
-
-        app.start()
     }
 
     render() {
