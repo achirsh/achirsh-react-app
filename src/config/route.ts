db109a5945af7e48436041df563f13d8720a401f
geqb <geqb@shanhai.team>
2022-11-24 15:53:37 +0800

    no message

--- src/config/route.ts
+++ src/config/route.ts
@@ -5,51 +5,7 @@ export const routes = [
         page: "home",
     },
     {
-        url: "home/map",
-        page: "map",
-    },
-    {
-        url: "home/me",
-        page: "me",
-    },
-    {
-        url: "home/building",
-        page: "building",
-    },
-    {
-        url: "home/building/setting",
-        page: "building/setting",
-    },
-    {
-        url: "home/square",
-        page: "square",
-    },
-    {
-        url: "home/square/release",
-        page: "square/release",
-    },
-    {
-        url: "home/wishing-well",
-        page: "wishing-well",
-    },
-    {
-        url: "home/tribal",
-        page: "tribal",
-    },
-    {
-        url: "home/tribal/chat",
-        page: "tribal/chat",
-    },
-    {
-        url: "home/three",
-        page: "three",
-    },
-    {
         url: "home/bili",
         page: "bili",
-    },
-    {
-        url: "home/ceshi",
-        page: "ceshi",
     },
 ]
--- src/pages/bili/index.tsx
+++ src/pages/bili/index.tsx
@@ -21,12 +21,12 @@ export default class Bili extends Component {
     private oldContainer = { x: 0, y: 0 }
 
     componentDidMount(): void {
-        const app = new PIXI.Application({
+        const app: any = new PIXI.Application({
             width: config.clientWidth(),
             height: config.clientHeight(),
             antialias: true, // 抗锯齿
             resolution: 2, // 分辨率
-            backgroundAlpha: 1, // 背景透明
+            backgroundAlpha: 1,
         })
 
         document.getElementById("bili-main")?.appendChild(app.view)
@@ -45,7 +45,7 @@ export default class Bili extends Component {
         }
 
         this.container.on("pointerdown", (event: any) => {
-            console.log("event.data.screen", event.data.screen)
+            console.log("event.data.screen", event.data.screen, event.data.global)
 
             // this.diff = {
             //     x: event.data.global.x - config.clientWidth() / 2,
@@ -64,39 +64,39 @@ export default class Bili extends Component {
             // }
         })
 
-        PIXI.Assets.load(String(PUBLIC_PATH) + "assets/bilibili/cat.json").then(spritesheet => {
-            const cat_down = []
-            const cat_left = []
-            const cat_right = []
-            const cat_up = []
+        // PIXI.Assets.load(String(PUBLIC_PATH) + "assets/bilibili/cat.json").then(spritesheet => {
+        //     const cat_down = []
+        //     const cat_left = []
+        //     const cat_right = []
+        //     const cat_up = []
 
-            for (let i = 0; i <= 8; i++) {
-                cat_down.push(PIXI.Texture.from(`cat_down_${i}.png`))
-                cat_left.push(PIXI.Texture.from(`cat_left_${i}.png`))
-                cat_right.push(PIXI.Texture.from(`cat_right_${i}.png`))
-                cat_up.push(PIXI.Texture.from(`cat_up_${i}.png`))
-            }
+        //     for (let i = 0; i <= 8; i++) {
+        //         cat_down.push(PIXI.Texture.from(`cat_down_${i}.png`))
+        //         cat_left.push(PIXI.Texture.from(`cat_left_${i}.png`))
+        //         cat_right.push(PIXI.Texture.from(`cat_right_${i}.png`))
+        //         cat_up.push(PIXI.Texture.from(`cat_up_${i}.png`))
+        //     }
 
-            dealAnim(cat_down, true)
+        //     dealAnim(cat_down, true)
 
-            dealAnim(cat_left, false)
+        //     dealAnim(cat_left, false)
 
-            dealAnim(cat_right, false)
+        //     dealAnim(cat_right, false)
 
-            dealAnim(cat_up, false)
+        //     dealAnim(cat_up, false)
 
-            function dealAnim(element: any, show: boolean) {
-                const anim: any = new PIXI.AnimatedSprite(element)
+        //     function dealAnim(element: any, show: boolean) {
+        //         const anim: any = new PIXI.AnimatedSprite(element)
 
-                anim.x = width / 2
-                anim.y = height / 2
-                anim.animationSpeed = 0.1
-                anim.width = 145 / divisor
-                anim.height = 145 / divisor
-                anim.visible = show
-                app.stage.addChild(anim)
-            }
-        })
+        //         anim.x = width / 2
+        //         anim.y = height / 2
+        //         anim.animationSpeed = 0.1
+        //         anim.width = 145 / divisor
+        //         anim.height = 145 / divisor
+        //         anim.visible = show
+        //         app.stage.addChild(anim)
+        //     }
+        // })
 
         app.stage.pivot.x = (width - config.clientWidth()) / 2
         app.stage.pivot.y = (height - config.clientHeight()) / 2
