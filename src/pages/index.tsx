3779d49ffff311a8ae06b57b533b4c7ef6d6bc45
geqb <geqb@shanhai.team>
2022-11-24 18:40:42 +0800

    修改public_path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function PageIndex(): JSX.Element {
 
     return (
         <Routes location={location}>
-            <Route path={String(PUBLIC_PATH)}>
+            <Route path={"/"}>
                 {config.routes.map(item => {
                     return (
                         <Route
